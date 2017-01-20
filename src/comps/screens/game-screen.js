550f2c412c9b4f05cf7c81105df5f166668b9cf9
chiptus <chiptus@gmail.com>
2017-01-20 17:52:46 +0100

    add game-screen comp

---
+++ src/comps/screens/game-screen.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+import DefaultLayout from '../layout/default-layout';
+import Game from  '../game/game';
+
+import buildGame from '../../lib/game';
+import words from '../../lib/words.json';
+
+
+class GameScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      game: buildGame(words, 0),
+      rightCount: 0,
+    }
+
+    this.handleDoHClick = this.handleDoHClick.bind(this);
+  }
+
+
+  componentWillMount() {
+    this.setState({ word: this.state.game.getNextWord() })
+  }
+
+
+  handleDoHClick(lid) {
+    let rightAnswer = this.state.game.checkAnswer(this.state.word.id, lid);
+    let rightCount = this.state.rightCount + (rightAnswer ? 1 : 0);
+    let word = this.state.game.getNextWord();
+    let finish = !word;
+
+    let toastMsg = rightAnswer ? 'Right!' : 'Wrong :(';
+    ToastAndroid.show(toastMsg, ToastAndroid.SHORT);
+
+    this.setState({
+      rightCount,
+      word,
+      showFinish: finish,
+    });
+  }
+
+  render() {
+    return (
+      <DefaultLayout>
+        <Game word={this.state.word.word} onClick={this.handleDoHClick} />
+      </DefaultLayout>
+    )
+  }
+}
+
+export default GameScreen
