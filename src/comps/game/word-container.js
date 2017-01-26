e1c3829597e4417b3fd8b321355c0d4e601dce54
chiptus <chiptus@gmail.com>
2017-01-26 19:20:49 +0100

    make word object move from side to side

--- src/comps/game/word-container.js
+++ src/comps/game/word-container.js
@@ -12,10 +12,20 @@ class WordContainer extends Component {
       { translateX: absoluteChangeX },
       //{ translateY: absoluteChangeY }
     ];
+    
+    // if (absoluteChangeX > 180) {
+    //   this.props.onOutside(1);
+    //   return null;
+    // }
+
+    // if (absoluteChangeX < -180) {
+    //   this.props.onOutside(0);
+    //   return null;
+    // }
 
     const {word} = this.props;
     return (
-      <View style={{ transform }}>
+      <View style={[ {transform} , styles.container]}>
         <Text style={styles.word}>{word[0].toUpperCase() + word.substr(1)}</Text>
       </View>
     )
@@ -23,6 +33,12 @@ class WordContainer extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    //width: 80,
+    backgroundColor: 'aqua',
+    borderRadius: 2,
+    elevation: 4,
+  },
   word: {
     fontSize: 50,
   },
