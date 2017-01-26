9f33aedf21e72f8a1f8903009da7145c40299911
chiptus <chiptus@gmail.com>
2017-01-26 18:50:36 +0100

    make word object movable

--- src/comps/game/word-container.js
+++ src/comps/game/word-container.js
@@ -1,13 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
+import makePannable from '../decorators/panning-decorator';
 
+@makePannable
 class WordContainer extends Component {
-  render () {
+  render() {
+    const {
+      absoluteChangeX, absoluteChangeY
+    } = this.props;
+    const transform = [
+      { translateX: absoluteChangeX },
+      //{ translateY: absoluteChangeY }
+    ];
+
     const {word} = this.props;
     return (
-      <Text style={styles.word}>{word[0].toUpperCase() + word.substr(1)}</Text>
-     )
-   }
+      <View style={{ transform }}>
+        <Text style={styles.word}>{word[0].toUpperCase() + word.substr(1)}</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
