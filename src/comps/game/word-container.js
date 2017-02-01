c3f7f50f0ccc1529288f5baabfdd1e02580331eb
chiptus <chiptus@gmail.com>
2017-02-01 16:56:57 +0100

    remove position change handlers

--- src/comps/game/word-container.js
+++ src/comps/game/word-container.js
@@ -10,22 +10,12 @@ class WordContainer extends Component {
     } = this.props;
     const transform = [
       { translateX: absoluteChangeX },
-      //{ translateY: absoluteChangeY }
     ];
-    
-    // if (absoluteChangeX > 180) {
-    //   this.props.onOutside(1);
-    //   return null;
-    // }
 
-    // if (absoluteChangeX < -180) {
-    //   this.props.onOutside(0);
-    //   return null;
-    // }
 
     const {word} = this.props;
     return (
-      <View style={[ {transform} , styles.container]}>
+      <View style={[{ transform }, styles.container]}>
         <Text style={styles.word}>{word[0].toUpperCase() + word.substr(1)}</Text>
       </View>
     )
