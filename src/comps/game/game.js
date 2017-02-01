f5817f7adb6ea0d4ef5ae9eaaba398cb23b93598
chiptus <chiptus@gmail.com>
2017-02-01 16:58:07 +0100

    add pan end handler to word

--- src/comps/game/game.js
+++ src/comps/game/game.js
@@ -5,16 +5,42 @@ import ContentContainer from '../layout/content-container';
 import WordContainer from './word-container';
 import DeOfHetContainer from './de-of-het-container';
 class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onPanEnd = this.onPanEnd.bind(this);
+  }
+
   render() {
     const {word, onClick} = this.props;
     return (
-      <View style={styles.container}>
+      <View style={styles.container} onLayout={this.onLayout}>
         <View style={styles.seperator} />
         <DeOfHetContainer />
-        <View style={styles.seperator} />        
-        <WordContainer word={word} onOutside={onClick}/>
+        <View style={styles.seperator} />
+        <WordContainer word={word} onPanEnd={this.onPanEnd} />
       </View>
     );
+  }
+
+  onLayout = ({ nativeEvent: { layout: {width, height} } }) => {
+    this.width = width;
+  }
+
+  getBorder() {
+    return (this.width / 2) - 50;
+  }
+
+  onPanEnd = ({absoluteChangeX, returnToDefaultLocation}) => {
+    returnToDefaultLocation();
+    if (absoluteChangeX > this.getBorder()) {
+      this.props.onClick(1);
+      return;
+    }
+    if (absoluteChangeX < -1 * this.getBorder()) {
+      this.props.onClick(0);
+      return;
+    }
   }
 }
 
