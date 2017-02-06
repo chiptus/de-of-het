33b0da3654d9bf1ab2c78f5de799587ec34e697c
chiptus <chiptus@gmail.com>
2017-02-06 07:17:35 +0100

    fix a bug in class object - checkAnswer
    checkAnswer throws exception when didn't play any words

--- src/lib/game.js
+++ src/lib/game.js
@@ -2,6 +2,9 @@ export default function buildGame(words = [], level = 0) {
   const playedWords = [];
 
   const checkAnswer = (id, doh) => {
+    if (!playedWords.length) {
+      return false;
+    }
     const word = playedWords.find(w => w.id === id);
     return (word.doh === doh);
   }
